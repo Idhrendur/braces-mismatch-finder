99db75e97ca7bb66efc0872fbda7c012441699f3
Idhrendur <idhrendur@hotmail.com>
2022-08-29 22:18:49 -0700

    Explicit ascii encoding

--- bracket_script.py
+++ bracket_script.py
@@ -19,7 +19,7 @@ for filename in glob.iglob(root_dir + '**/*.txt', recursive=True):
     closing_bracket_count = 0
 
     try:
-        with open(filename, 'r') as file:
+        with open(filename, 'r', encoding='ascii') as file:
             for line in file:
                 cleaned_line = remove_comments(line, '#')
                 opening_bracket_count += cleaned_line.count('{')
